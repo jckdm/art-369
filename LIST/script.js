22697084cfac30d2201234ffb65c563a87b5a12b
Jack Adam <jackadam@Jacks-MBP-3.wireless.yale.internal>
2020-01-25 16:15:10 -0500

    jk too fast

--- LIST/script.js
+++ LIST/script.js
@@ -27,7 +27,7 @@ async function ani() {
     document.body.style.backgroundColor = pantone[i%pantone.length];
     document.getElementById('pan').innerHTML = pantone_names[i%pantone.length];
     links[i%10].style.color = pantone[(i+1)%pantone.length];
-    await sleep(1000);
+    await sleep(3000);
     i++;
     yr.innerHTML = yrs[i%pantone.length];
   }
