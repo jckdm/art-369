f9d2afff112dfd7c788837775e7de861a978becf
Jack Adam <jackadam@Jacks-MBP-3.wireless.yale.internal>
2020-02-06 23:50:56 -0500

    list 23 + js

---
+++ LIST/23script.js
@@ -0,0 +1,14 @@
+window.onload = function() { alert("Press the up and down arrow keys."); };
+
+let i = 0;
+document.addEventListener('keyup', (e) => {
+  if (e.code === "ArrowDown") {
+    if (i > 0) { hide(i-1) }
+    if (i < 20) { reveal(i); i++; }}
+  else if (e.code === "ArrowUp") {
+    if (i < 20) { hide(i); }
+    if ((i <= 20) && (i > 0)) { reveal(i-1); i--; }}
+});
+
+function hide(i) { document.getElementById(i.toString()).style.display = "none"; }
+function reveal(i) { document.getElementById(i.toString()).style.display = "block"; }
