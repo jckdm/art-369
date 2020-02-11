db08d7517c3496292d31e0731a403aa18ed5d2f5
Jack Adam <jackadam@Jacks-MBP-3.wireless.yale.internal>
2020-02-10 20:05:28 -0500

    bug fix: list 23

--- LIST/23script.js
+++ LIST/23script.js
@@ -3,11 +3,13 @@ window.onload = function() { alert("Press the up and down arrow keys."); };
 let i = 0;
 document.addEventListener('keyup', (e) => {
   if (e.code === "ArrowDown") {
+    console.log(i);
     if (i > 0) { hide(i-1) }
-    if (i < 20) { reveal(i); i++; }}
+    if (i <= 20) { reveal(i); i++; }}
   else if (e.code === "ArrowUp") {
-    if (i < 20) { hide(i); }
-    if ((i <= 20) && (i > 0)) { reveal(i-1); i--; }}
+    console.log(i);
+    if (i <= 20) { hide(i); }
+    if ((i <= 21) && (i > 0)) { reveal(i-1); i--; }}
 });
 
 function hide(i) { document.getElementById(i.toString()).style.display = "none"; }
