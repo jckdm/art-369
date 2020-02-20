a5d2021937ce2856824e2eaaf88e83b153668f2b
Jack Adam <jackadam@Jacks-MBP-3.wireless.yale.internal>
2020-02-20 18:37:32 -0500

    ok lets try this on the www

--- CLOCK/script.js
+++ CLOCK/script.js
@@ -1,8 +1,9 @@
 function play() {
-  var r = document.getElementById("radio");
-  var a = document.getElementById("audio");
+  var r = document.getElementById('radio');
+  var a = document.getElementById('audio');
+  var b = document.getElementById('light');
   var x = parseInt(r.attributes.x.value);
 
-  if (x == 700) { r.attributes.x.value = 715; a.play(); }
-  else { r.attributes.x.value = 700; a.pause(); }
+  if (x == 700) { r.attributes.x.value = 715; a.play(); b.attributes.fill.value = "#008000"; }
+  else { r.attributes.x.value = 700; a.pause(); b.attributes.fill.value = "#b30000"; }
 }
